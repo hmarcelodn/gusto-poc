1d759944cfc50a2b0bbf378a9c10d22274e170a2
Marcelo Del Negro <h.marcelo.del.negro@outlook.com>
2018-12-27 15:27:05 -0300

    adding foundation theme + custom meeting post-type

---
+++ wp-content/mu-plugins/meeting-post-types.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Register meeting post-type
+ */
+function meeting_post_types(){
+    register_post_type('meeting', array(
+        //'supports' => array('', '', ''),
+        'rewrite' => array('slug' => 'meetings'),
+        'public' => true,
+        'labels' => array(
+            'name' => 'Meetings',
+            'add_new_item' => 'Add New Meeting',
+            'edit_item' => 'Edit Meeting',
+            'all_items' => 'All Meetings',
+            'singular_name' => 'Meeting'
+        ),
+        'menu_icon' => 'dashicons-calendar'
+    ));
+}
+
+add_action('init', 'meeting_post_types');
